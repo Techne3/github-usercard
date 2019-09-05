044788439d536bbe33ae6e83c7c573ba5b25815f
Timothy Sharkey <Techne3@gmail.com>
2019-09-05 16:39:48 -0600

    layout

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -52,4 +52,120 @@ const followersArray = [];
   justsml
   luishrd
   bigknell
-*/
+
+
+  */
+ 
+
+
+  const container = document.querySelector('.cards')
+
+    function gitCard (data) {
+        const newCard = document.createElement('div')
+        const newImg = document.createElement('img');
+        const info = document.createElement('div');
+        const title = document.createElement('h3');
+        const userName = document.createElement('p');
+        const location = document.createElement('p');
+        const profile = document.createElement('p');
+        const profileA = document.createElement('a');
+        const followers = document.createElement('p')
+        const following = document.createElement('p')
+        const bio = document.createElement('p')
+
+
+      
+      newCard.appendChild(newImg);
+      newCard.appendChild(info);
+      info.appendChild(userName);
+      info.appendChild(title)
+      info.appendChild(location);
+      info.appendChild(profileA)
+        profileA.appendChild(profile)
+      info.appendChild(followers)  
+      info.appendChild(following)
+      info.appendChild(bio)
+
+      newCard.classList.add('card');
+      title.classList.add('name')
+      info.classList.add('card-info')
+      userName.classList.add('name');
+      
+
+      // data 
+      newImg.src = `${data.avatar_url}`
+      title.textContent=`${data.login}`
+      userName.textContent=`${data.name}`
+      location.textContent=`${data.location}`
+      profile.textContent=`Profile: ${data.html_url} `
+      profileA.href =`${data.html_url}`
+      profileA.textContent=`${data.html_url}`
+      followers.textContent=`Followers: ${data.followers}`
+      following.textContent=`Following: ${data.following}`
+      bio.textContent =`Bio: ${data.bio}` 
+
+
+      return newCard
+    }
+    // const entryPoint = document.querySelector(".entry")
+
+//     <div class="card">
+//      <img src={image url of user} />
+//   <div class="card-info">
+//     <h3 class="name">{users name}</h3>
+//     <p class="username">{users user name}</p>
+//     <p>Location: {users location}</p>
+//     <p>Profile:  
+//       <a href={address to users github page}>{address to users github page}</a>
+//     </p>
+//     <p>Followers: {users followers count}</p>
+//     <p>Following: {users following count}</p>
+//     <p>Bio: {users bio}</p>
+//   </div>
+// </div>
+
+
+// let followersArray = ['tetondan'];
+ // My Github account data
+    let loginArray = ['seanaleid','DannyManzietti','crsullivan']
+    
+    axios.get('https://api.github.com/users')
+    .then(response => {
+      console.log(response.data)
+      loginArray.forEach( items => {
+        console.log(items)
+        container.appendChild(gitCard(`https://api.github.com/users/ ${items}`))
+      })
+    })
+
+
+
+
+    axios.get('https://api.github.com/users/Techne3')
+    .then(response => {
+      console.log(response.data)
+      container.appendChild(gitCard(response.data))
+    })
+    .catch(error=>{
+      console.log("This looks wrong", error)
+     } )
+      
+
+    //looping through my gitHub followers api 
+    // make an array to loop through
+    axios
+    .get('https://api.github.com/users/Techne3/followers')
+    .then(response =>{
+      console.log(response);
+      console.log(response.data)
+      followersArray = response.data;
+        followersArray.forEach(data =>{
+        container.appendChild(gitCard(data))
+       })
+    })
+    .catch(error => {
+      console.log('this is an ERROR', error)
+    })
+ 
+
+ 
